084db21260ee2c078a9cc0e17a80e2bdee108dfd
fangqjj <fangqjj@126.com>
2025-05-30 11:24:54 +0800

    test SavedModelBundleFactory::InternalCreateSavedModelBundle

--- tensorflow_serving/servables/tensorflow/saved_model_bundle_factory.cc
+++ tensorflow_serving/servables/tensorflow/saved_model_bundle_factory.cc
@@ -152,6 +152,8 @@ Status SavedModelBundleFactory::InternalCreateSavedModelBundle(
           result.config.mutable_experimental()->mutable_session_metadata();
       session_metadata->set_name(metadata->servable_id.name);
       session_metadata->set_version(metadata->servable_id.version);
+    } else {
+      LOG(INFO) << "InternalCreateSavedModelBundle without metadata";
     }
     return result;
   }();
